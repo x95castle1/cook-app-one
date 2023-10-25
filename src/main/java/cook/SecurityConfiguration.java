035a813854dc15ebb229aece58dc58a52500117b
Arul  Khanna Vannala <41644359+arulvannala@users.noreply.github.com>
2023-10-25 14:34:55 -0600

    Update SecurityConfiguration.java

--- src/main/java/cook/SecurityConfiguration.java
+++ src/main/java/cook/SecurityConfiguration.java
@@ -23,7 +23,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@Profile("stg")
+@Profile("development")
 public class SecurityConfiguration {
 
     @Bean
